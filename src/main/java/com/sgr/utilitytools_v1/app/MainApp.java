02a66ac1ed8ecded3be03da8243a12126c740c63
Thayrony Von Held <thayronyvonheld@gmail.com>
2026-01-21 20:52:02 -0300

    Funcionando

--- src/main/java/com/sgr/utilitytools_v1/app/MainApp.java
+++ src/main/java/com/sgr/utilitytools_v1/app/MainApp.java
@@ -3,18 +3,20 @@ package com.sgr.utilitytools_v1.app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
-
     @Override
-    public void start(Stage stage) throws Exception {
-
+    public void start(Stage mainStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("/com/sgr/UtilityTools_v1/main/main.fxml"));
+                getClass().getResource("/com/sgr/utilitytools_v1/view/main/main.fxml"));
 
         Parent root = loader.load();
-        stage.show();
+
+        mainStage.setScene(new Scene(root, 1024, 600));
+        mainStage.setTitle("Utility Tools");
+        mainStage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/com/sgr/utilitytools_v1/app/MainController.java
+++
@@ -1,5 +0,0 @@
-package com.sgr.utilitytools_v1.app;
-
-public class MainController {
-
-}
