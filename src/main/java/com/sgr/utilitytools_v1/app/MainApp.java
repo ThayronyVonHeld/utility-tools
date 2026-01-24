1109e4475a5790e77e7f32f22cfca17dead76ccc
Thayrony Von Held <thayronyvonheld@gmail.com>
2026-01-24 01:36:36 -0300

    Update

--- src/main/java/com/sgr/utilitytools_v1/app/MainApp.java
+++ src/main/java/com/sgr/utilitytools_v1/app/MainApp.java
@@ -2,26 +2,21 @@ package com.sgr.utilitytools_v1.app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
-
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage mainStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/com/sgr/utilitytools_v1/view/main/main.fxml"));
 
-        var url = MainApp.class.getResource(
-                "/com/sgr/utilitytools_v1/dashboard/dashboard.fxml"
-        );
+        Parent root = loader.load();
 
-        System.out.println("FXML URL = " + url);
-
-        FXMLLoader loader = new FXMLLoader(url);
-        Scene scene = new Scene(loader.load());
-
-        stage.setScene(scene);
-        stage.setTitle("Utility Tools");
-        stage.show();
+        mainStage.setScene(new Scene(root, 1024, 600));
+        mainStage.setTitle("Utility Tools");
+        mainStage.show();
     }
 
     public static void main(String[] args) {
