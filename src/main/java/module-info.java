f76bd07afdab218f546375ebf034992ec707d0b8
Thayrony Von Held <thayronyvonheld@gmail.com>
2026-03-22 16:06:44 -0300

    Update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module com.sgr.utilitytools_v1 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires com.jfoenix;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.fontawesome6;
 
     opens com.sgr.utilitytools_v1.app to javafx.fxml;
     opens com.sgr.utilitytools_v1.dashboard to javafx.fxml;
