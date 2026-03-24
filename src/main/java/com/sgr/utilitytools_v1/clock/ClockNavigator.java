c4f3a907f93d307d92f9404137085b4229053e62
Thayrony Von Held <thayronyvonheld@gmail.com>
2026-03-24 16:22:08 -0300

    Update

--- src/main/java/com/sgr/utilitytools_v1/clock/ClockNavigator.java
+++ src/main/java/com/sgr/utilitytools_v1/clock/ClockNavigator.java
@@ -7,10 +7,6 @@ import javafx.scene.layout.StackPane;
 public class ClockNavigator {
 
     public static void loadClockView(StackPane pane, String fxml) {
-        if (pane == null) {
-            System.err.println("Erro: O StackPane de destino está nulo!");
-            return;
-        }
         try {
             Parent root = FXMLLoader.load(ClockNavigator.class.getResource(fxml));
             pane.getChildren().setAll(root);
