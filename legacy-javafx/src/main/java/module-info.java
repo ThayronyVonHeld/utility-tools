1ca952bec1e8dbd7084c350cd66ff352df1ac4d0
Thayrony Von Held <thayronyvonheld@gmail.com>
2026-04-18 22:34:47 -0300

    Update Module info

--- legacy-javafx/src/main/java/module-info.java
+++ legacy-javafx/src/main/java/module-info.java
@@ -1,10 +1,12 @@
 module com.sgr.utilitytools_v1 {
+    requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
     requires com.jfoenix;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.ikonli.fontawesome6;
+
 
     opens com.sgr.utilitytools_v1.app to javafx.fxml;
     opens com.sgr.utilitytools_v1.dashboard to javafx.fxml;
