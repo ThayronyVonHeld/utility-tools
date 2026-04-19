70f411cdf4006be50c5eb74d630c22fb1e448472
Thayrony Von Held <thayronyvonheld@gmail.com>
2026-04-18 22:10:49 -0300

    Update

--- legacy-javafx/src/main/java/com/sgr/utilitytools_v1/clock/timer/TimerController.java
+++ legacy-javafx/src/main/java/com/sgr/utilitytools_v1/clock/timer/TimerController.java
@@ -1,11 +1,9 @@
 package com.sgr.utilitytools_v1.clock.timer;
 
-import com.sgr.utilitytools_v1.clock.ClockNavigator;
 import com.sgr.utilitytools_v1.clock.ClockService;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Arc;
 
--- legacy-javafx/src/main/java/com/sgr/utilitytools_v1/dashboard/DashboardController.java
+++ legacy-javafx/src/main/java/com/sgr/utilitytools_v1/dashboard/DashboardController.java
@@ -1,12 +1,8 @@
 package com.sgr.utilitytools_v1.dashboard;
 
 
-import com.sgr.utilitytools_v1.navigation.Navigator;
 import javafx.fxml.FXML;
 import javafx.scene.layout.StackPane;
-
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class DashboardController {
 
