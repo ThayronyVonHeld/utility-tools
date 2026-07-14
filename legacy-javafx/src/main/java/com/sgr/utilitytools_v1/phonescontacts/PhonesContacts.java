74ec252db07562a76e144c7142d3e301b3056d10
Thayrony Von Held <thayronyvonheld@gmail.com>
2026-07-14 14:26:21 -0300

    Update

--- legacy-javafx/src/main/java/com/sgr/utilitytools_v1/phonescontacts/PhonesContacts.java
+++ legacy-javafx/src/main/java/com/sgr/utilitytools_v1/phonescontacts/PhonesContacts.java
@@ -1,4 +1,9 @@
 package com.sgr.utilitytools_v1.phonescontacts;
 
+import java.util.ArrayList;
+
 public class PhonesContacts {
+    ArrayList<String> name = new ArrayList<>();
+    ArrayList<String> contatos = new ArrayList<>();
+
 }
